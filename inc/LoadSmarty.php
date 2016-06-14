8dd58e9745d66a4709dafdf77e1199181fd294ee
Peter Snoek <petersnoek@davinci.nl>
2016-06-14 11:18:11 +0200

    make sure files in cache and templates_c folders are not added to git

--- inc/LoadSmarty.php
+++ inc/LoadSmarty.php
@@ -2,11 +2,11 @@
 
 date_default_timezone_set('Europe/Amsterdam');
 
-require_once ('/inc/smarty/Smarty.class.php');
+require_once ('smarty/Smarty.class.php');
 
 $smarty = new Smarty();
-$smarty->setTemplateDir('/tpl/');
-$smarty->setCompileDir('/inc/smarty/templates_c/');
-$smarty->setConfigDir('/inc/smarty/configs/');
-$smarty->setCacheDir('/inc/smarty/cache/');
+$smarty->setTemplateDir('tpl/');
+$smarty->setCompileDir('inc/smarty/templates_c/');
+$smarty->setConfigDir('inc/smarty/configs/');
+$smarty->setCacheDir('inc/smarty/cache/');
 
